7c7f8984a087e0aca7d81215b27ac37d90413328
Bob Li <bobli8203@gmail.com>
2025-05-28 22:56:22 -0400

    Fixed stupidity in Project.v

--- src/project.v
+++ src/project.v
@@ -29,9 +29,9 @@ module tt_um_uwasic_onboarding_bob_li (
       .clk(clk),
       .COPI(ui_in[1]),
       .SCLK(ui_in[0]),
-      .nCS(ui_in[2])
+      .nCS(ui_in[2]),
       .rst_n(rst_n),
-      .en_reg_out_7_0(en_reg_out_7_0)
+      .en_reg_out_7_0(en_reg_out_7_0),
       .en_reg_out_15_8(en_reg_out_15_8),
       .en_reg_pwm_7_0(en_reg_pwm_7_0),
       .en_reg_pwm_15_8(en_reg_pwm_15_8),
